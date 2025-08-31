7d762a346783ae52e6ecda7bed91ef7d8e04c092
Richard Everley <hello@richardeverley.com>
2025-09-01 09:22:35 +1000

    - Feature: Implement bonus requirement `Implement request rate limiting`

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit'
 import { taskRoutes } from './routes/taskRoutes';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: {
+    error: {
+      message: "Too many requests from this IP, please try again later.",
+      status: 429,
+      timestamp: new Date().toISOString(),
+    },
+  },
+});
+
+// Apply the rate limiter to all requests.
+app.use(limiter);
 
 // Middleware
 app.use(helmet());
